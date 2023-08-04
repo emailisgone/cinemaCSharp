47fcb634b75a98464f2f91f0ccc1df597e2d4739
emailisgone <nikita.exe0@gmail.com>
2023-08-04 10:22:56 +0300

    Add files via upload

---
+++ CinemaAdmin/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace CinemaAdmin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.roomsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.moviesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sessionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dgvAdmin = new System.Windows.Forms.DataGridView();
+            this.cMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAdmin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.roomsToolStripMenuItem,
+            this.moviesToolStripMenuItem,
+            this.sessionsToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(746, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // roomsToolStripMenuItem
+            // 
+            this.roomsToolStripMenuItem.Name = "roomsToolStripMenuItem";
+            this.roomsToolStripMenuItem.Size = new System.Drawing.Size(69, 24);
+            this.roomsToolStripMenuItem.Text = "Rooms";
+            this.roomsToolStripMenuItem.Click += new System.EventHandler(this.roomsToolStripMenuItem_Click);
+            // 
+            // moviesToolStripMenuItem
+            // 
+            this.moviesToolStripMenuItem.Name = "moviesToolStripMenuItem";
+            this.moviesToolStripMenuItem.Size = new System.Drawing.Size(70, 24);
+            this.moviesToolStripMenuItem.Text = "Movies";
+            this.moviesToolStripMenuItem.Click += new System.EventHandler(this.moviesToolStripMenuItem_Click);
+            // 
+            // sessionsToolStripMenuItem
+            // 
+            this.sessionsToolStripMenuItem.Name = "sessionsToolStripMenuItem";
+            this.sessionsToolStripMenuItem.Size = new System.Drawing.Size(78, 24);
+            this.sessionsToolStripMenuItem.Text = "Sessions";
+            this.sessionsToolStripMenuItem.Click += new System.EventHandler(this.sessionsToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(54, 24);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // dgvAdmin
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Silver;
+            this.dgvAdmin.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvAdmin.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvAdmin.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvAdmin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAdmin.Location = new System.Drawing.Point(0, 31);
+            this.dgvAdmin.Name = "dgvAdmin";
+            this.dgvAdmin.RowHeadersWidth = 51;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dgvAdmin.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvAdmin.RowTemplate.Height = 24;
+            this.dgvAdmin.Size = new System.Drawing.Size(746, 446);
+            this.dgvAdmin.TabIndex = 1;
+            this.dgvAdmin.CellMouseDown += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvAdmin_CellMouseDown);
+            this.dgvAdmin.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvAdmin_CellValueChanged);
+            this.dgvAdmin.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.dgvAdmin_UserDeletingRow);
+            this.dgvAdmin.MouseMove += new System.Windows.Forms.MouseEventHandler(this.dgvAdmin_MouseMove);
+            // 
+            // cMenu
+            // 
+            this.cMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.cMenu.Name = "cMenu";
+            this.cMenu.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 477);
+            this.Controls.Add(this.dgvAdmin);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAdmin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem roomsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem moviesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sessionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dgvAdmin;
+        private System.Windows.Forms.ContextMenuStrip cMenu;
+    }
+}
+
---
+++ CinemaAdmin/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ApplicationLayer;
+using ApplicationLayer.AdminTools;
+
+namespace CinemaAdmin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dgvAdmin.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+        List<DataGridViewRow> updatedRows = new List<DataGridViewRow>();
+        //List<DataGridViewRow> insertedRows = new List<DataGridViewRow>();
+        List<ulong> deletedIndex = new List<ulong>();
+        //List<ulong> insertedIndex = new List<ulong>();
+        List<string> insertData;
+        bool isThereInsertData = false;
+        private string tableName;
+        bool sessionTable = false;
+        int mouseX;
+        int mouseY;
+        string currentMenuStrip;
+        
+        private void FormatDgv()
+        {
+            dgvAdmin.Columns[0].ReadOnly = true;    //cannot change ID
+            tableName = (dgvAdmin.DataSource as DataTable).TableName;
+            //check if picture cell exists
+            foreach (DataGridViewColumn item in dgvAdmin.Columns)
+            {
+                if (item.GetType() == typeof(DataGridViewImageColumn))
+                {
+                    (item as DataGridViewImageColumn).ImageLayout = DataGridViewImageCellLayout.Zoom;
+                }
+            }
+        }
+
+        private void roomsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dgvAdmin.AllowUserToAddRows = true;
+            dgvAdmin.DataSource = AdminDataTools.GetAllRooms();
+            FormatDgv();
+            sessionTable = false;
+            currentMenuStrip = "room";
+        }
+
+        private void dgvAdmin_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            //Checking if the list has this row
+            var found = updatedRows.FindAll(x => x.Cells[0].Value == dgvAdmin[0, e.RowIndex].Value);
+            if (found == null)
+            {
+                updatedRows.Add(dgvAdmin.Rows[e.RowIndex]);
+                return;
+            }
+            //If row was found
+            updatedRows.Remove(dgvAdmin.Rows[e.RowIndex]);
+            updatedRows.Add(dgvAdmin.Rows[e.RowIndex]);
+            Console.WriteLine("==============test==============");
+            foreach (var item in updatedRows)
+            {
+                Console.WriteLine(item.Cells[0].Value + " " + item.Cells[1].Value);
+            }
+        }
+
+        private void dgvAdmin_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+            deletedIndex.Add((ulong)dgvAdmin[0, e.Row.Index].Value);
+            updatedRows.RemoveAll(x=>(ulong)x.Cells[0].Value==deletedIndex[deletedIndex.Count-1]);
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Checking for empty rows
+            insertData = new List<string>();
+            Console.WriteLine("Null cells:");
+            //List<string> data = new List<string>();
+            /*foreach (var item in updatedRows)
+            {
+                Console.WriteLine(item.Cells[1].Value);
+                for(int i=0; i<item.Cells.Count; i++)
+                {
+                    data.Add(item.Cells[i].Value.ToString());
+                }
+                if(item.Cells[0].Value == null || item.Cells[0].Value == DBNull.Value)
+                {
+                    
+                    switch (currentMenuStrip)
+                    {
+                        case "room":
+                            AdminDataTools.RowsToAdd(currentMenuStrip, data);
+                            break;
+                        case "movie":
+                            break;
+                        case "session":
+                            break;
+                    }
+                }
+            }
+            foreach (var item in data)
+            {
+                Console.WriteLine(item);
+            }*/
+            dgvAdmin.AllowUserToAddRows = false;
+            foreach (DataGridViewRow row in dgvAdmin.Rows)
+            {
+                if (row.Cells[0].Value == null || row.Cells[0].Value == DBNull.Value)
+                {
+                    //Console.WriteLine(row.Cells[1].Value + " " + row.Cells[2].Value);
+                    if (currentMenuStrip == "room")
+                    {
+                        //MessageBox.Show(row.Cells[2].Value.ToString());
+                        insertData.Add(row.Cells[1].Value.ToString());
+                        insertData.Add(Convert.ToInt32(row.Cells[2].Value).ToString());
+                    }
+                    isThereInsertData = true;
+                    dgvAdmin.AllowUserToAddRows = true;
+                    break;
+                }
+            }
+            if (dgvAdmin.EndEdit())
+            {
+                if (deletedIndex.Count == 0 && updatedRows.Count == 0) return;
+                if (AdminDataTools.UpdateDeleteData(deletedIndex, updatedRows, tableName))
+                {
+                    Console.WriteLine("Save OK!");
+                    deletedIndex.Clear();
+                    updatedRows.Clear();
+                }
+                if (isThereInsertData)
+                {
+                    if(AdminDataTools.GetInsertData(currentMenuStrip, insertData))
+                    {
+                        insertData.Clear();
+                        isThereInsertData = false;
+                        dgvAdmin.AllowUserToAddRows = true;
+                    }
+                }
+            }
+        }
+
+        private void moviesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dgvAdmin.DataSource = AdminDataTools.GetAllMovies();
+            FormatDgv();
+            sessionTable = false;
+            currentMenuStrip = "movie";
+        }
+
+        private void sessionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dgvAdmin.DataSource = AdminDataTools.GetAllSessions();
+            FormatDgv();
+            sessionTable = true;
+            currentMenuStrip = "session";
+        }
+
+        private void dgvAdmin_CellMouseDown(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            dgvAdmin.EndEdit();
+            if (!sessionTable) return;
+            //If movie title column
+            if (e.Button == MouseButtons.Right)
+            {
+                if (dgvAdmin.Columns[e.ColumnIndex].HeaderText == "Title")
+                {
+                    FillContextMenu("Title", e.ColumnIndex, e.RowIndex);             
+                }
+                else if (dgvAdmin.Columns[e.ColumnIndex].HeaderText == "Name")
+                {
+                    FillContextMenu("Name", e.ColumnIndex, e.RowIndex);
+                }
+            }
+        }
+
+        private void FillContextMenu(string header, int column, int row)
+        {
+            List<string> lst = new List<string>();
+            if (header == "Title")
+            {
+                lst = AdminDataTools.GetMovieTitles();
+            }
+            else if(header == "Name")
+            {
+                lst = AdminDataTools.GetRoomNames();
+            }
+            cMenu.Items.Clear();
+            foreach (var item in lst)
+            {
+                var data = new ToolStripMenuItem();
+                data.Text = item;
+                data.Click += delegate (object sender, EventArgs e)
+                {
+                    dgvAdmin.CurrentCell.Value = data.Text;
+                };
+                cMenu.Items.Add(data);
+            }
+
+            cMenu.Hide();
+            foreach (DataGridViewCell item in dgvAdmin.SelectedCells)
+            {
+                item.Selected = false;
+            }
+            dgvAdmin[column, row].Selected = true;
+            dgvAdmin.CurrentCell = dgvAdmin[column, row];
+            cMenu.Show(dgvAdmin, mouseX, mouseY);
+        }
+
+        private void dgvAdmin_MouseMove(object sender, MouseEventArgs e)
+        {
+            mouseX = e.X;
+            mouseY = e.Y;
+        }
+    }
+}
